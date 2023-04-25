6ff94d792a002765be34391ac8bf5825bc2f8d9d
Lukasz Sliwinski <luki3141@gmail.com>
2023-04-25 14:41:06 +0200

    Changes

--- Code/gs.py
+++ Code/gs.py
@@ -7,7 +7,6 @@ import torch
 import matplotlib.pyplot as plt
 
 # Implement agents
-
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
@@ -33,6 +32,9 @@ class MLP(nn.Module):
             nn.Linear(2, 64),
             nn.ReLU(),
             nn.Linear(64, 32),
+            nn.Dropout(),
+            nn.ReLU(),
+            nn.Linear(32, 32),
             nn.ReLU(),
             nn.Linear(32, 1)
         )
@@ -138,7 +140,7 @@ def MDP(agent1, agent2, env, batch_size, T, device):
     """Perform episode simulation to obtain the rewards"""
     running_rewards = torch.zeros(size=(1, 2), device=device)
     state = torch.randn(size=(batch_size, 2),
-                                requires_grad=False, device=device)
+                        requires_grad=False, device=device)*2+2
     for t in range(T):
         action1 = agent1.get_action(state)
         action2 = agent2.get_action(state)
@@ -150,63 +152,63 @@ def MDP(agent1, agent2, env, batch_size, T, device):
     return running_rewards, actions[0, :]
 
 
-# Define agents
-agent1 = CalvanoAgent(0.01, device, 0.99)
-agent2 = DummyAgent(1.5)
-env = CalvanoTorch(np.array([1., 1.]), 1, 1., np.array([0., 0.]), device)
+# # Define agents
+# agent1 = CalvanoAgent(0.01, device, 0.99)
+# agent2 = DummyAgent(1.5)
+# env = CalvanoTorch(np.array([1., 1.]), 1, 1., np.array([0., 0.]), device)
 
-T = 20
-batch_size = 20
-learning_steps = 50
+# T = 20
+# batch_size = 20
+# learning_steps = 50
 
-# Run learning
-for i in range(learning_steps):
-    agent1.zero_gradients()
-    running_rewards, actions = MDP(agent1, agent2, env, batch_size, T, device)
-    running_rewards[0, 0].backward(retain_graph=False)
-    agent1.update()
-
-
-actions = torch.linspace(0, 10, 100).reshape(-1, 1)
-prices = torch.hstack((actions, actions*0 + 1.5))
-rewards = env.step(prices)
-
-# print(rewards)
-plt.plot(actions, rewards[:, 0].flatten())
-plt.show()
+# # Run learning
+# for i in range(learning_steps):
+#     agent1.zero_gradients()
+#     running_rewards, actions = MDP(agent1, agent2, env, batch_size, T, device)
+#     running_rewards[0, 0].backward(retain_graph=False)
+#     agent1.update()
 
 
-exit(0)
-# We start with defining two agents
-outer_agent = CalvanoAgent(0.0005, device)
-# inner agent has to learn faster than the outer
-# inner_agent = CalvanoAgent(0.01, device)
+# actions = torch.linspace(0, 10, 100).reshape(-1, 1)
+# prices = torch.hstack((actions, actions*0 + 1.5))
+# rewards = env.step(prices)
 
-# We define parameters
-T = 20
-batch_size = 5
-inner_learning_steps = 30
-outer_learning_steps = 50
+# # print(rewards)
+# plt.plot(actions, rewards[:, 0].flatten())
+# plt.show()
 
-# Outer loop
-for outer_it in range(outer_learning_steps):
-    outer_running_reward = torch.Tensor([0])
-    outer_agent.zero_gradients()
-    # Inner loop
-    inner_agent = CalvanoAgent(0.01, device)
-    for inner_it in range(inner_learning_steps):
-        inner_agent.zero_gradients()
-        # Get rewards
-        running_rewards, last_actions = MDP(inner_agent, outer_agent,
-                                            env, batch_size, T, device)
-        inner_running_reward = torch.Tensor([0]) + running_rewards[0, 0]
-        inner_running_reward.backward(retain_graph=True)
-        outer_running_reward += running_rewards[0, 1]
-        last_outer_reward = torch.Tensor([0]) + running_rewards[0, 1]
-        # Update inner agent
-        inner_agent.update()
-    # Print results
-    print('outer iteration ', outer_it)
-    print(inner_running_reward.item(), last_outer_reward.item(),
-          last_actions.detach().numpy())
-    outer_agent.update()
+
+# exit(0)
+# # We start with defining two agents
+# outer_agent = CalvanoAgent(0.0005, device)
+# # inner agent has to learn faster than the outer
+# # inner_agent = CalvanoAgent(0.01, device)
+
+# # We define parameters
+# T = 20
+# batch_size = 5
+# inner_learning_steps = 30
+# outer_learning_steps = 50
+
+# # Outer loop
+# for outer_it in range(outer_learning_steps):
+#     outer_running_reward = torch.Tensor([0])
+#     outer_agent.zero_gradients()
+#     # Inner loop
+#     inner_agent = CalvanoAgent(0.01, device)
+#     for inner_it in range(inner_learning_steps):
+#         inner_agent.zero_gradients()
+#         # Get rewards
+#         running_rewards, last_actions = MDP(inner_agent, outer_agent,
+#                                             env, batch_size, T, device)
+#         inner_running_reward = torch.Tensor([0]) + running_rewards[0, 0]
+#         inner_running_reward.backward(retain_graph=True)
+#         outer_running_reward += running_rewards[0, 1]
+#         last_outer_reward = torch.Tensor([0]) + running_rewards[0, 1]
+#         # Update inner agent
+#         inner_agent.update()
+#     # Print results
+#     print('outer iteration ', outer_it)
+#     print(inner_running_reward.item(), last_outer_reward.item(),
+#           last_actions.detach().numpy())
+#     outer_agent.update()
--- Code/gs_single.py
+++ Code/gs_single.py
@@ -1,4 +1,59 @@
-from gs import CalvanoAgent, CalvanoTorch
+"""Check training simple Monte-Carlo agent
+with the Calvano environment and a dummy agent.
+"""
+
+from gs import CalvanoAgent, CalvanoTorch, DummyAgent, MDP
 import matplotlib.pyplot as plt
 
 import torch
+import numpy as np
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Device is {device}')
+
+
+
+if __name__=="__main__":
+    # Define agents
+    dummy_price = 1.5
+    agent1 = CalvanoAgent(0.01, device, 0.995)
+    agent2 = DummyAgent(dummy_price)
+    env = CalvanoTorch(np.array([1., 1.]), 1, 1., np.array([0., 0.]), device)
+
+    # Define learning hyperparameters
+    T = 6
+    batch_size = 30
+    learning_steps = 300
+
+    # Run learning
+    for i in range(learning_steps):
+        agent1.zero_gradients()
+        running_rewards, actions = MDP(agent1, agent2, env, batch_size, T, device)
+        running_rewards[0, 0].backward(retain_graph=False)
+        agent1.update()
+
+
+    # Get sample history
+    actions = torch.linspace(0, 5, 100).reshape(-1, 1)
+    prices = torch.hstack((actions, actions*0 + 1.5))
+    rewards = env.step(prices)
+
+
+    plt.plot(actions, rewards[:, 0].flatten())
+    plt.xlabel('actions')
+    plt.ylabel('rewards')
+    plt.title('rewards for each action')
+    plt.savefig(f'rewards_dummy_{dummy_price}.png', dpi=300)
+    plt.close()
+
+
+    # Get sample actions
+    states = torch.hstack((actions, actions*0 + 1.5))
+    agent_action = agent1.get_action(state=states)
+    plt.plot(actions.flatten(), agent_action.detach().numpy().flatten())
+    plt.xlabel('state (previous action)')
+    plt.ylabel('action')
+    plt.title(f'Next actions based on previous action \n and dummy action {dummy_price}')
+    plt.savefig(f'action_dummy_{dummy_price}.png', dpi=300)
+    plt.close()
